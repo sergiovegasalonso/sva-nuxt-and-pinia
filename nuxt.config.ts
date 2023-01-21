ea30557a6d3f10face810f349ec97caa016a5069
sergio <sergio.vegas.alonso@gmail.com>
2023-01-21 13:45:26 +0100

    COOnfig pinia ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,17 @@
 export default defineNuxtConfig({
+    // ... other options
+    modules: [
+        // ...
+        [
+            '@pinia/nuxt',
+            {
+                autoImports: [
+                    // automatically imports `defineStore`
+                    'defineStore', // import { defineStore } from 'pinia'
+                    // automatically imports `defineStore` as `definePiniaStore`
+                    ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+                ],
+            },
+        ],
+    ],
 })

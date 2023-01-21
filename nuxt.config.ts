27c40b1fb3e3ce451ac6ff497af6b26d00034036
sergio <sergio.vegas.alonso@gmail.com>
2023-01-21 13:46:19 +0100

    Configuration cleaned.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 export default defineNuxtConfig({
-    // ... other options
     modules: [
-        // ...
         [
             '@pinia/nuxt',
             {
                 autoImports: [
-                    // automatically imports `defineStore`
                     'defineStore', // import { defineStore } from 'pinia'
-                    // automatically imports `defineStore` as `definePiniaStore`
                     ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
                 ],
             },
